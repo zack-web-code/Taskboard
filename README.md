29448e16f6cf0eebe4a9f969b9e8fc156e3d3857
Khethukhanyo Mbekeni <khethukhanyom@gmail.com>
2026-02-18 20:46:21 +0200

    Initial commit: Graded Lab0, Git initialized, instructions followed

---
+++ README.md
@@ -0,0 +1,5 @@
+C:\Users\kheth>node -v
+v24.13.1
+
+C:\Users\kheth>npm -v
+11.8.0
---
+++ hello-node.js
@@ -0,0 +1,9 @@
+const Information= {
+    name: "Khethukhanyo",
+    surname: "Mbekeni",
+    age: 20,
+    institution: 'UJ',
+    course: 'BIT'
+};
+
+console.log(Information);
